1b01d23654823f2581fea6a91f0bfe5594d21585
DevRuiz18 <gustavosruiz@gmail.com>
2023-02-02 20:06:38 -0300

    null treatment

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,7 +5,12 @@ export abstract class View<T>{
     private escape = false;
 
     constructor(selector: string, escape?: boolean){
-        this.elemento = document.querySelector(selector);
+        const elemento =  document.querySelector(selector);
+        if(elemento){
+            this.elemento = elemento as HTMLElement;
+        }else{
+            throw Error(`Seletor ${selector} não existe no DOM. Verifique com o desenvolvedor`);
+        }
         if(escape){
             this.escape = escape;
         }
